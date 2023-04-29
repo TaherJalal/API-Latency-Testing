cd33199167d0cd641542e4618a400c12b503fb0b
TaherJalal <taherjalaljuma@gmail.com>
2023-04-29 16:18:11 +0300

    remove api key

--- go/server.go
+++ go/server.go
@@ -16,7 +16,7 @@ func main() {
 	var responses []string
 
 	for i := 0; i < 1000; i++ {
-		resp, err := http.Get("https://api.themoviedb.org/3/discover/movie?api_key=010b85a5594b639d99d3ea642bd45c74&language=en-US&sort_by=original_title.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate")
+		resp, err := http.Get("")
 		if err != nil {
 			fmt.Println("Error:", err)
 		}
