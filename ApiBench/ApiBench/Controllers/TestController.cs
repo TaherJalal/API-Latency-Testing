9eeef206e8ae70cf4d6f196dc37b6b0d8d91110b
TaherJalal <taherjalaljuma@gmail.com>
2023-04-29 16:16:59 +0300

    added c# project

---
+++ ApiBench/ApiBench/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiBench.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("test")]
+        public async Task<IActionResult> Index()
+        {
+            List<string> responses = new List<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string url = "";
+                    UriBuilder builder = new UriBuilder(url);
+                    HttpResponseMessage response = await client.GetAsync(builder.Uri);
+
+                    string content = await response.Content.ReadAsStringAsync();
+                    responses.Add(content);
+                }
+            }
+            return Ok(responses);
+        }
+    }
+}
