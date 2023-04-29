8c95b6fd098226e8dcb3a1361b24964c1fc5c076
TaherJalal <taherjalaljuma@gmail.com>
2023-04-29 16:19:42 +0300

    remove api key

--- python/server.py
+++ python/server.py
@@ -9,7 +9,7 @@ app = FastAPI()
 responses = []
 
 for i in range(1000):
-    response = requests.get("https://api.themoviedb.org/3/discover/movie?api_key=010b85a5594b639d99d3ea642bd45c74&language=en-US&sort_by=original_title.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate")
+    response = requests.get("")
     responses.append(response.content.decode("utf-8"))
 
 @app.get("/")
