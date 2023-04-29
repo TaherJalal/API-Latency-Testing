395faa64168e40b1daf59b5563ca5d9523b17651
TaherJalal <taherjalaljuma@gmail.com>
2023-04-29 16:19:06 +0300

    remove api key

--- typescript/server.ts
+++ typescript/server.ts
@@ -7,7 +7,7 @@ const app:Express = express();
 app.get("/" , async (req: Request , res: Response) => {
     let list:any = []
     for(let i = 0 ; i<=1000; i++ ){
-        const response:any = await axios.get("https://api.themoviedb.org/3/discover/movie?api_key=010b85a5594b639d99d3ea642bd45c74&language=en-US&sort_by=original_title.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate")
+        const response:any = await axios.get("")
         list.push(response.data)
     }
 
